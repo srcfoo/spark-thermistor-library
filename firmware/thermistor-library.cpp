0547cc439f7cb6401a36840deefdff18e9696343
Eric Peterson <srcfoo@users.noreply.github.com>
2016-09-16 20:42:56 -0500

    Adding serial debugging

--- firmware/thermistor-library.cpp
+++ firmware/thermistor-library.cpp
@@ -36,6 +36,9 @@ float Thermistor::getTempK(bool smooth) {
 	_temp_k = log(((40960000/_temp_raw) - _resistor));
 	_temp_k = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * _temp_k * _temp_k ))* _temp_k);
 	
+	Serial.print("Temp K: ");
+	Serial.println(_temp_k);
+	
 	return _temp_k;
 }
 
@@ -45,6 +48,9 @@ float Thermistor::getTempC(bool smooth) {
 	
 	_temp_c = _temp_k - 273.15;
 
+	Serial.print("Temp C: ");
+	Serial.println(_temp_c);
+	
 	return _temp_c;
 }
 
@@ -54,6 +60,9 @@ float Thermistor::getTempF(bool smooth) {
 	
 	_temp_f = (_temp_c * 9.0)/ 5.0 + 32.0;
 
+	Serial.print("Temp F: ");
+	Serial.println(_temp_f;
+	
 	return _temp_f;
 }
 
